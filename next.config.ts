a0108bf04ec5b63320f5481713a459b2bde9e9cd
Juancho2706 <jvillegas.dev@gmail.com>
2026-01-10 16:21:37 -0300

    Optimizacion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/segundapagina",
+        destination: "https://gymtrainer-a2g6feaoo-juancho2706s-projects.vercel.app/segundapagina",
+      },
+      {
+        source: "/segundapagina/:path*",
+        destination: "https://gymtrainer-a2g6feaoo-juancho2706s-projects.vercel.app/segundapagina/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
